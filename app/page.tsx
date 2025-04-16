81c80161a705d9b0b62dd3f4bd02fe138c181c3e
OrcDev <urosmiric88@gmail.com>
2025-04-16 14:01:10 +0200

    feat(page) add responsive text size and nowrap for main heading on mobile devices

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,9 @@ export default function Home() {
     <div className="absolute w-full h-full">
       <Lightning hue={220} xOffset={0} speed={1} intensity={1} size={1} />
       <div className="absolute flex flex-col gap-5 justify-center items-center top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
-        <h1 className="text-5xl font-bold text-white">IT IS COMING</h1>
+        <h1 className="md:text-5xl text-3xl font-bold text-white text-nowrap">
+          IT IS COMING
+        </h1>
         <TheHordeForm />
       </div>
     </div>
