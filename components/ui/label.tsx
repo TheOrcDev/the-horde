62f5c44b8a362957230599252ce1ac5e2144059f
OrcDev <urosmiric88@gmail.com>
2025-12-10 11:34:05 +0100

    refactor(components) update Shadcn components

--- components/ui/label.tsx
+++ components/ui/label.tsx
@@ -1,21 +1,24 @@
-"use client";
+"use client"
 
-import { cva } from "class-variance-authority";
-import { Label as LabelPrimitive } from "radix-ui";
-import type * as React from "react";
+import * as React from "react"
+import * as LabelPrimitive from "@radix-ui/react-label"
 
-import { cn } from "@/lib/utils";
+import { cn } from "@/lib/utils"
 
-const labelVariants = cva(
-  "font-medium text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
-);
-
-const Label = ({
+function Label({
   className,
   ...props
-}: React.LabelHTMLAttributes<HTMLLabelElement>) => (
-  <LabelPrimitive.Root className={cn(labelVariants(), className)} {...props} />
-);
-Label.displayName = LabelPrimitive.Root.displayName;
+}: React.ComponentProps<typeof LabelPrimitive.Root>) {
+  return (
+    <LabelPrimitive.Root
+      data-slot="label"
+      className={cn(
+        "flex items-center gap-2 text-sm leading-none font-medium select-none group-data-[disabled=true]:pointer-events-none group-data-[disabled=true]:opacity-50 peer-disabled:cursor-not-allowed peer-disabled:opacity-50",
+        className
+      )}
+      {...props}
+    />
+  )
+}
 
-export { Label };
+export { Label }
